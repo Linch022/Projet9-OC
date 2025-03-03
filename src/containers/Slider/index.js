efe31808ea61c1d450ce2586f96a0aa92fc02a50
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-03-03 19:48:06 +0100

    Correction des soucis sur les bullets Points : -> sortie de la logique du map à l'intérieur d'un autre map -> modification de la key

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -23,7 +23,6 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
           <div
             key={event.title}
             className={`SlideCard SlideCard--${
@@ -39,20 +38,20 @@ const Slider = () => {
               </div>
             </div>
           </div>
+
+      ))}
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc?.map((e, radioIdx) => (
                 <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
+                key={`radio-${e.title}`}
+                type="radio"
+                name="radio-button"
+                checked={index === radioIdx}
                 />
               ))}
             </div>
           </div>
-        </>
-      ))}
     </div>
   );
 };
